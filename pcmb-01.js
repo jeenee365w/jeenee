ea144f915298a9c5afc98801aec03a78a054860f
jeenee365w <jeenee365w@gmail.com>
2025-04-03 09:29:15 +0530

    Update pcmb-01.js

--- pcmb-01.js
+++ pcmb-01.js
@@ -1,6 +1,14 @@
-const students = {
+// Sem 01 & 02 
+const students_01_02 = {
     "abhranil@gmail.com":"jn-01",
-    "nil@gmail.com":"jn-02"
+    "nil2":"jn-03",
+
+}
+
+// Sem 01 
+const students_01 = {
+    "nil@gmail.com":"jn-02",
+    "nil":"jn-04",
 
 }
 
@@ -11,7 +19,7 @@ const emailf = document.getElementById('email');
 const butf = document.getElementById('but');
 
 function f1() {
-    if (emailf.value in students && students[emailf.value] === fname.value) {
+    if (emailf.value in students_01_02 && students_01_02[emailf.value] === fname.value || emailf.value in students_01 && students_01[emailf.value] === fname.value) {
         window.location.href = 'pcmb-01.html';
     }else if (emailf.value === '') {}
     
